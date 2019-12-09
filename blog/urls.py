a647cdc7f3777c79b50347c872f3d40a907e8133
fanhao <alan.fan.h@qq.com>
2019-12-09 16:37:23 +0800

    更改第7行代码，将url更改为path

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    url(r'^$', views.blog_title,  name="blog_title"),
+    path('', views.blog_title,  name="blog_title"),
     path('<int:article_id>', views.blog_article, name="blog_detail")
 ]
