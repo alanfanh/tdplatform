e0a5127b8b6807970db1e42f12de898cf316b3e0
fanhao <alan.fan.h@qq.com>
2019-12-09 16:32:57 +0800

    修改BlogArticles模型，增加了group字段关联account应用Group表

--- blog/models.py
+++ blog/models.py
@@ -3,16 +3,18 @@ from django.db import models
 # Create your models here.
 from django.utils import timezone
 from django.contrib.auth.models import User
+from account.models import Group
 
 class BlogArticles(models.Model):
     title = models.CharField(max_length=300)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE , related_name="blog_posts")
+        User, on_delete=models.CASCADE, related_name="blog_posts")
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
 
     class meta:
         ordering = ("-publish",)
-    
+
     def __str__(self):
         return self.title
