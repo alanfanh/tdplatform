4f8388001b9fa78727919ac132a247153bd064fe
fanhao <alan.fan.h@qq.com>
2020-01-19 14:30:58 +0800

    修复模型中拼写错误的餐宿

--- article/models.py
+++ article/models.py
@@ -8,7 +8,7 @@ from slugify import slugify
 
 class ArticleColumn(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User,on_delete=models.DO_NOTHING ,related_name="article_column")
     column = models.CharField(verbose_name="栏目", max_length=50)
     created = models.DateField(auto_now_add=True)
 
@@ -17,7 +17,7 @@ class ArticleColumn(models.Model):
 
 class ArticlePost(models.Model):
     id = models.AutoField(primary_key=True)
-    auther = models.ForeignKey(User, verbose_name="作者", on_delete=models.DO_NOTHING, related_name="article")
+    author = models.ForeignKey(User, verbose_name="作者", on_delete=models.DO_NOTHING, related_name="author_article")
     title = models.CharField(verbose_name="标题", max_length=100)
     slug = models.SlugField(max_length=500)
     column = models.ForeignKey(ArticleColumn, verbose_name="栏目", on_delete=models.CASCADE)
@@ -33,8 +33,8 @@ class ArticlePost(models.Model):
         return self.title
     
     def save(self, *args, **kargs):
-        self.sug = slugify(self.title)
-        super(ArticlePost, self).save(*args, **kwargs)
+        self.slug = slugify(self.title)
+        super(ArticlePost, self).save(*args, **kargs)
 
     def get_absolute_url(self):
         return reverse("article:article_detail", args=[self.id,self.slug])
