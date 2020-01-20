a5aa3e1a3f3febcd8701eddff41449746c65e889
fanhao <alan.fan.h@qq.com>
2020-01-20 17:09:21 +0800

    增加post、list、detail等url和相关视图函数

--- article/views.py
+++ article/views.py
@@ -8,7 +8,9 @@ from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 
 from .models import ArticleColumn
-from .forms import ArticleColumnForm
+from .models import ArticlePost
+from .forms import ArticleColumnForm, ArticlePostForm
+from django.shortcuts import get_object_or_404
 
 @login_required(login_url='/account/login/')
 @csrf_exempt
@@ -51,3 +53,45 @@ def del_article_column(request):
         return HttpResponse("1")
     except:
         return HttpResponse('2')
+
+@login_required(login_url='/accpunt/login')
+@csrf_exempt
+def article_post(request):
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            cd = article_post_form.cleaned_data
+            try:
+                new_article = article_post_form.save(commit=False)
+                new_article.author = request.user
+                new_article.column = request.user.article_column.get(id=request.POST['column_id'])
+                new_article.save()
+                return HttpResponse("1")
+            except:
+                return HttpResponse("2")
+        else:
+            return HttpResponse("3")
+    else:
+        article_post_form = ArticlePostForm()
+        article_columns = request.user.article_column.all()
+        return render(request, "article/column/article_post.html", {"article_post_form":article_post_form, "article_columns":article_columns})
+
+@login_required(login_url="/account/login")
+def article_list(request):
+    articles = ArticlePost.objects.filter(author=request.user)
+    return render(request, "article/column/article_list.html", {"articles":articles})
+
+@login_required(login_url="account/login")
+def article_detail(request, id, slug):
+    article = get_object_or_404(ArticlePost, id=id, slug=slug)
+    return render(request, "article/column/article_detail.html", {"article":article})
+
+@login_required(login_url="/account/login")
+def del_article(request):
+    article_id = request.POST['article_id']
+    try:
+        article = ArticlePost.objects.filter(id=article_id)
+        article.delete()
+        return HttpResponse("1")
+    except:
+        return HttpResponse("2")
