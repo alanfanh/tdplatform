2fda60828a47ec802cc0ee65e590d21c26c094b8
fanhao <alan.fan.h@qq.com>
2020-02-24 16:43:05 +0800

    修改登录表单，增加一些可定制内容

--- account/forms.py
+++ account/forms.py
@@ -3,8 +3,10 @@ from django import forms
 from .models import UserInfo
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': u'请输入用户名', }))
+    password = forms.CharField(
+        error_messages={'required': u'密码不能为空'}, widget=forms.PasswordInput)
 
 class UserInfoForm(forms.Form):
     class Meta:
