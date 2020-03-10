527ca083c38932d90e9d45491128a5621a271610
fanhao <alan.fan.h@qq.com>
2020-03-10 17:47:12 +0800

    完善个人中心我贡献的培训页面，并增加侧边菜单栏url

--- training/urls.py
+++ training/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.urls import path
-from .views import CourseListView 
+from .views import CourseListView, PersonCourseListView
 
 app_name="training"
 urlpatterns = [
-    path('course-list/',CourseListView.as_view(), name="course_list")
+    path('course-list/',CourseListView.as_view(), name="course_list"),
+    path('person-course/', PersonCourseListView.as_view(),name="person_course")
 ]
--- training/views.py
+++ training/views.py
@@ -5,8 +5,28 @@ from django.shortcuts import get_object_or_404,render
 
 from django.views.generic import TemplateView, ListView
 from .models import Course, Integral
+from account.models import UserInfo
 
 class CourseListView(ListView):
     model = Course
     context_object_name = "courses"
     template_name = 'training/course_list.html'
+
+
+class UserMixin(object):
+    def get_queryset(self):
+        qs = super(UserMixin, self).get_queryset()
+        return qs.filter(teacher=UserInfo.objects.get(user=self.request.user))
+
+class UserCourseMixin(UserMixin):
+    model = Course
+
+class PersonCourseListView(UserCourseMixin, ListView):
+    # 我贡献的培训视图
+    context_object_name = "courses"
+    template_name = "training/person_course_list.html"
+
+    def get_context_data(self, **kwargs):
+        # 获取context传入模版，如下为userinfo
+        kwargs['userinfo'] = UserInfo.objects.get(user=self.request.user)
+        return super(PersonCourseListView, self).get_context_data(**kwargs)
