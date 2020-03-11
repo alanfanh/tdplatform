cdbd323e568f00d8f2579818740c1b31c4119daa
fanhao <alan.fan.h@qq.com>
2020-03-11 17:35:04 +0800

    “增加视图和url

--- asset/urls.py
+++ asset/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.article_title,  name="blog_title"),
     path('<int:article_id>', views.article_content, name="blog_detail"),
     # 
-    path('my-complaint/', views.my_complaint, name="my_complaint")
+    path('my-complaint/', views.my_complaint, name="my_complaint"),
+    path('unprocess-tec/', views.unprocess_tec, name="unprocess_tec"),
+    path('process-tec/', views.process_tec, name="process_tec"),
 ]
--- asset/views.py
+++ asset/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 # Create your views here.
-from .models import Articles
-from account.models import UserInfo
+from .models import Articles, TecContent, Complaint
+from account.models import UserInfo, Role, Group
+
+from django.http import HttpResponse
 
 @login_required(login_url="/account/login")
 def article_title(request):
@@ -24,3 +26,32 @@ def article_content(request, article_id):
 def my_complaint(request):
     userinfo = UserInfo.objects.get(user=request.user)
     return render(request,"asset/pl_complaint_list.html",{"userinfo":userinfo})
+
+@login_required(login_url="/account/login")
+def unprocess_tec(request):
+    # 未处理的优秀实践
+    userinfo = UserInfo.objects.get(user=request.user)
+    role = Role.objects.get(id=userinfo.role_id)
+    if role.role_name == "PL":
+        # PL用户
+        tec_list = TecContent.objects.filter(status="1")
+        return render(request, "asset/unprocess_pl.html",{"userinfo":userinfo, "tec_list":tec_list})
+    elif role.role_name == "M":
+        # M用户
+        tec_list = TecContent.objects.filter(status="2")
+        return render(request, "asset/unprocess_m.html", {"userinfo":userinfo,"tec_list":tec_list})
+    else:
+        return HttpResponse('Not Found')
+
+@login_required(login_url="account/login")
+def process_tec(request):
+    # 已处理的优秀实践
+    userinfo =  UserInfo.objects.get(user=request.user)
+    role = Role.objects.get(id=userinfo.role_id)
+    if role.role_name == "PL":
+        tec_list = TecContent.objects.filter()
+        return render(request, "asset/processed_pl.html",{"userinfo":userinfo, "tec_list":tec_list})
+    elif role.role_name == "M":
+        return render(request, "asset/processed_m.html", {"userinfo":userinfo})
+    else:
+        return HttpResponse("Not Found")
