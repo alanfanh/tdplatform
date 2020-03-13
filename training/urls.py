424fd25808ef7801b9c68213b68dcf2e4662b299
fanhao <alan.fan.h@qq.com>
2020-03-13 10:57:25 +0800

    新增添加course页面，同时继承LoginRequiredMixin

--- training/urls.py
+++ training/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.urls import path
 from .views import CourseListView, PersonCourseListView
+from .views import CourseCreateView
 
 app_name="training"
 urlpatterns = [
     path('course-list/',CourseListView.as_view(), name="course_list"),
-    path('person-course/', PersonCourseListView.as_view(),name="person_course")
+    path('person-course/', PersonCourseListView.as_view(),name="person_course"),
+    path('add-course/', CourseCreateView.as_view(), name="add_course"),
 ]
--- training/views.py
+++ training/views.py
@@ -3,14 +3,19 @@ from django.shortcuts import get_object_or_404,render
 # Create your views here.
 # 本views.py文件不再写视图函数，而是通过类的视图实现功能
 
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, CreateView
 from .models import Course, Integral
 from account.models import UserInfo
+from braces.views import LoginRequiredMixin
+from django.shortcuts import redirect
+from .forms import CreateCourseForm
 
-class CourseListView(ListView):
+
+class CourseListView(LoginRequiredMixin, ListView):
     model = Course
     context_object_name = "courses"
     template_name = 'training/course_list.html'
+    login_url = "/account/login/"
 
 
 class UserMixin(object):
@@ -18,8 +23,9 @@ class UserMixin(object):
         qs = super(UserMixin, self).get_queryset()
         return qs.filter(teacher=UserInfo.objects.get(user=self.request.user))
 
-class UserCourseMixin(UserMixin):
+class UserCourseMixin(UserMixin, LoginRequiredMixin):
     model = Course
+    login_url="/account/login/"
 
 class PersonCourseListView(UserCourseMixin, ListView):
     # 我贡献的培训视图
@@ -30,3 +36,17 @@ class PersonCourseListView(UserCourseMixin, ListView):
         # 获取context传入模版，如下为userinfo
         kwargs['userinfo'] = UserInfo.objects.get(user=self.request.user)
         return super(PersonCourseListView, self).get_context_data(**kwargs)
+
+class CourseCreateView(CreateView):
+    model = Course
+    fields = ['cname', 'range', 'course_time', 'address', 'teacher','file_name']
+    template_name = "training/add_course.html"
+
+    def Post(self, request, *args, **kwargs):
+        form = CreateCourseForm(data=request.POST)
+        if form.is_valid():
+            new_course = form.save(commit=False)
+            new_course.author = self.request.user
+            new_course.save()
+            return redirect("training:course_list")
+        return self.render_to_response({"form":form})
