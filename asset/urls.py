e25478fd39d3b01a5fe3c4209e64d1a8d8cf1d2f
fanhao <alan.fan.h@qq.com>
2020-03-16 17:42:02 +0800

    增加添加客诉和优秀实践的url

--- asset/urls.py
+++ asset/urls.py
@@ -12,6 +12,12 @@ urlpatterns = [
     path('process-tec/', views.process_tec, name="process_tec"),
     path('my-tec/', views.my_tec, name="my_tec"),
     path('assigned-list', views.assigned_list, name="assigned_list"),
+    # 主页客诉
     path('complaint-list/', views.complaint_list, name="complaint_list"),
     path('complaint-detail/<int:complaint_id>', views.complaint_detail, name="complaint_detail"),
+    path('add-complaint/', views.add_complaint, name="add_complaint"),
+    # 主页优秀实践
+    path('tec-list/', views.tec_list, name="tec_list"),
+    path('tec-detail/', views.tec_detail, name="tec_detail"),
+    path('add-tec', views.add_tec, name="add_tec"),
 ]
