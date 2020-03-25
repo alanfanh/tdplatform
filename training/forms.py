10b2c670590f37760c360e81575389c447b5cd3a
fanhao <alan.fan.h@qq.com>
2020-03-25 16:07:42 +0800

    修改course表单，增加描述字段选项

--- training/forms.py
+++ training/forms.py
@@ -4,4 +4,4 @@ from .models import Course
 class CreateCourseForm(forms.ModelForm):
     class Meta:
         model = Course
-        fields = ("cname","range","course_time","address","teacher","file_name","student")
+        fields = ("cname","range","course_time","address","teacher","cdescription","file_name","student")
