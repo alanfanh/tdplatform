6fc52fb0c9bb555ca65a129e668e86e4cc2b790b
fanhao <alan.fan.h@qq.com>
2020-03-28 14:07:16 +0800

    增加下载课程附件的视图函数和url、增加删除课程视图类

--- training/urls.py
+++ training/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from .views import CourseListView, PersonCourseListView
 from .views import CourseCreateView, PointListView
 from .views import CourseDetailView
+from . import views
 
 app_name="training"
 urlpatterns = [
     path('course-list/',CourseListView.as_view(), name="course_list"),
     path('course-detail/<int:pk>', CourseDetailView.as_view(), name="course_detail"),
-    path('person-course/', PersonCourseListView.as_view(),name="person_course"),
+    path('person-course/', PersonCourseListView.as_view(), name="person_course"),
     path('add-course/', CourseCreateView.as_view(), name="add_course"),
+    path('download-coursefile/<int:course_id>', views.download_file, name="download_file"),
     path('point-list/', PointListView.as_view(), name="point_list"),
 ]
--- training/views.py
+++ training/views.py
@@ -3,25 +3,19 @@ from django.shortcuts import get_object_or_404,render
 # Create your views here.
 # 本views.py文件不再写视图函数，而是通过类的视图实现功能
 
-from django.views.generic import TemplateView, ListView, CreateView, DetailView
+from django.views.generic import TemplateView, ListView, CreateView, DetailView, DeleteView
 from .models import Course, Integral
 from account.models import UserInfo, Group
 from braces.views import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.shortcuts import redirect
 from .forms import CreateCourseForm
-
-
-class CourseListView(LoginRequiredMixin, ListView):
-    model = Course
-    context_object_name = "courses"
-    template_name = 'training/course_list.html'
-    login_url = "/account/login/"
-
-    def get_context_data(self, **kwargs):
-        kwargs['userinfo'] = UserInfo.objects.get(user=self.request.user)
-        return super(CourseListView, self).get_context_data(**kwargs)
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, FileResponse
+from django.utils.encoding import escape_uri_path
 
 class UserMixin(object):
+    # 筛选teacher为自己的course
     def get_queryset(self):
         qs = super(UserMixin, self).get_queryset()
         return qs.filter(teacher=UserInfo.objects.get(user=self.request.user))
@@ -65,16 +59,53 @@ class CourseCreateView(UserCourseMixin, CreateView):
             userinfos = UserInfo.objects.all()
             return self.render_to_response({"form": form, "userinfos":userinfos, "groups":groups})
 
+class CourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    context_object_name = "courses"
+    template_name = 'training/course_list.html'
+    login_url = "/account/login/"
+
+    def get_context_data(self, **kwargs):
+        kwargs['userinfo'] = UserInfo.objects.get(user=self.request.user)
+        return super(CourseListView, self).get_context_data(**kwargs)
 
 class CourseDetailView(LoginRequiredMixin, DetailView):
     model = Course
     template_name = "training/course_detail.html"
     login_url = "/account/login/"
 
+@login_required(login_url="/account/login")
+def download_file(request, course_id):
+    # 下载课程附件
+    course = Course.objects.get(id=course_id)
+    if course.file_name == "":
+        return HttpResponse('Null')
+    else:
+        file_name = course.file_name.name.split('/')[1]
+        response = FileResponse(course.file_name)
+        response['Content-Type'] = "application/octet-stream"
+        # 浏览器会导致中文乱码, 进行如下解码
+        response['Content-Disposition'] = "attachment; filename*=utf-8''{}".format(
+            escape_uri_path(file_name))
+        print(response['Content-Disposition'], content, file_name)
+    return response
+
+class DeleteCourseView(LoginRequiredMixin, DeleteView):
+    model = Course
+    login_url = "/account/login/"
+    success_url = reverse_lazy("training:course_list")
+
+    def dispatch(self, *args, **kwargs):
+        # 通过ajax弹窗删除
+        resp = super(DeleteCourseView, self).dispatch(*args, **kwargs)
+        if self.request.is_ajax():
+            response_data = {"result":"ok"}
+            return HttpResponse(json.dumps(response_data),content_type="application/json")
+        return resp
 
 # 积分视图
 class PointListView(LoginRequiredMixin, ListView):
     model = Integral
     context_object_name = "points"
     template_name = "training/point_list.html"
-    login_url = "account/login"
+    login_url = "/account/login"
