a2adabb6ee847d444c021315f608f4e8342626b9
fanhao <alan.fan.h@qq.com>
2020-05-12 16:09:09 +0800

    编辑article应用__init__文件增加其应用配置

---
+++ article/__init__.py
@@ -0,0 +1,17 @@
+from django.apps import AppConfig
+import os
+
+# 修改training应用在后台管理中的名字为中文
+
+verbose_app_name = '技术文章'
+
+default_app_config = 'article.ArticleConfig'
+
+
+def get_current_app_name(_file):
+    return os.path.split(os.path.dirname(_file))[-1]
+
+
+class ArticleConfig(AppConfig):
+    name = get_current_app_name(__file__)
+    verbose_name = verbose_app_name
