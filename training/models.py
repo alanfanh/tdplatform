b554675ca379a8dcde6690a5200b5280e0ce0bf6
fanhao <alan.fan.h@qq.com>
2020-07-28 10:59:11 +0800

    修改models中的外键关联为account.UserInfo

--- training/models.py
+++ training/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from account.models import UserInfo
+# from account.models import UserInfo
 
 # Create your models here.
 # 培训管理应用
@@ -10,11 +10,11 @@ class Course(models.Model):
     range = models.CharField(verbose_name="课程范围", max_length=50)
     course_time = models.DateTimeField(verbose_name="培训时间", auto_now=False, auto_now_add=False)
     address = models.CharField(verbose_name="培训地点", max_length=30)
-    teacher = models.ForeignKey(UserInfo, related_name="course_teacher",verbose_name="主讲人", on_delete=models.DO_NOTHING)
+    teacher = models.ForeignKey(to="account.UserInfo", related_name="course_teacher",verbose_name="主讲人", on_delete=models.DO_NOTHING)
     file_name = models.FileField(verbose_name="附件", upload_to="course_files", max_length=100)
     cdescription = models.CharField(verbose_name="课程概述",max_length=150, blank=True)
     # 多对多，培训课程与参加人
-    student = models.ManyToManyField(UserInfo, related_name="course_joined", blank=True)
+    student = models.ManyToManyField(to="account.UserInfo", related_name="course_joined", blank=True)
 
     class Meta:
         ordering = ('-course_time',)
@@ -28,7 +28,7 @@ class Integral(models.Model):
     """培训积分"""
     id = models.AutoField(primary_key=True)
     # 积分表与用户应该是一对一关系
-    person = models.ForeignKey(UserInfo, verbose_name="姓名", on_delete=models.CASCADE, related_name='person_integral')
+    person = models.ForeignKey(to="account.UserInfo", verbose_name="姓名", on_delete=models.CASCADE, related_name='person_integral')
     total = models.IntegerField(verbose_name="总积分")
     joined = models.IntegerField(verbose_name="参加培训积分")
     teached_group = models.IntegerField(verbose_name="组织小组培训积分")
